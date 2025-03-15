f2b82eeb6d17c335d1d72b1c8d2c4b9dc9787a5b
Seth Williamson <sethjwilliamson@gmail.com>
2025-03-15 15:12:05 -0600

    Update Generator3D.cs

--- Assets/Scripts3D/Generator3D.cs
+++ Assets/Scripts3D/Generator3D.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 using Random = System.Random;
 using Graphs;
 
-public class Generator3D : MonoBehaviour {
-    enum CellType {
+public class Generator3D : MonoBehaviour
+{
+    enum CellType
+    {
         None,
         Room,
         Hallway,
         Stairs
     }
 
-    class Room {
+    class Room
+    {
         public BoundsInt bounds;
 
-        public Room(Vector3Int location, Vector3Int size) {
+        public Room(Vector3Int location, Vector3Int size)
+        {
             bounds = new BoundsInt(location, size);
         }
 
-        public static bool Intersect(Room a, Room b) {
+        public static bool Intersect(Room a, Room b)
+        {
             return !((a.bounds.position.x >= (b.bounds.position.x + b.bounds.size.x)) || ((a.bounds.position.x + a.bounds.size.x) <= b.bounds.position.x)
                 || (a.bounds.position.y >= (b.bounds.position.y + b.bounds.size.y)) || ((a.bounds.position.y + a.bounds.size.y) <= b.bounds.position.y)
                 || (a.bounds.position.z >= (b.bounds.position.z + b.bounds.size.z)) || ((a.bounds.position.z + a.bounds.size.z) <= b.bounds.position.z));
@@ -47,19 +52,27 @@ public class Generator3D : MonoBehaviour {
     Delaunay3D delaunay;
     HashSet<Prim.Edge> selectedEdges;
 
-    void Start() {
+    void Start()
+    {
+        StartCoroutine(GenerateDungeon());
+    }
+
+    IEnumerator GenerateDungeon()
+    {
         random = new Random(0);
         grid = new Grid3D<CellType>(size, Vector3Int.zero);
         rooms = new List<Room>();
 
-        PlaceRooms();
+        yield return StartCoroutine(PlaceRooms());
         Triangulate();
-        CreateHallways();
-        PathfindHallways();
+        yield return StartCoroutine(CreateHallways());
+        yield return StartCoroutine(PathfindHallways());
     }
 
-    void PlaceRooms() {
-        for (int i = 0; i < roomCount; i++) {
+    IEnumerator PlaceRooms()
+    {
+        for (int i = 0; i < roomCount; i++)
+        {
             Vector3Int location = new Vector3Int(
                 random.Next(0, size.x),
                 random.Next(0, size.y),
@@ -76,8 +89,10 @@ public class Generator3D : MonoBehaviour {
             Room newRoom = new Room(location, roomSize);
             Room buffer = new Room(location + new Vector3Int(-1, 0, -1), roomSize + new Vector3Int(2, 0, 2));
 
-            foreach (var room in rooms) {
-                if (Room.Intersect(room, buffer)) {
+            foreach (var room in rooms)
+            {
+                if (Room.Intersect(room, buffer))
+                {
                     add = false;
                     break;
                 }
@@ -85,35 +100,42 @@ public class Generator3D : MonoBehaviour {
 
             if (newRoom.bounds.xMin < 0 || newRoom.bounds.xMax >= size.x
                 || newRoom.bounds.yMin < 0 || newRoom.bounds.yMax >= size.y
-                || newRoom.bounds.zMin < 0 || newRoom.bounds.zMax >= size.z) {
+                || newRoom.bounds.zMin < 0 || newRoom.bounds.zMax >= size.z)
+            {
                 add = false;
             }
 
-            if (add) {
+            if (add)
+            {
                 rooms.Add(newRoom);
-                PlaceRoom(newRoom.bounds.position, newRoom.bounds.size);
+                yield return StartCoroutine(PlaceRoom(newRoom.bounds.position, newRoom.bounds.size));
 
-                foreach (var pos in newRoom.bounds.allPositionsWithin) {
+                foreach (var pos in newRoom.bounds.allPositionsWithin)
+                {
                     grid[pos] = CellType.Room;
                 }
             }
         }
     }
 
-    void Triangulate() {
+    void Triangulate()
+    {
         List<Vertex> vertices = new List<Vertex>();
 
-        foreach (var room in rooms) {
+        foreach (var room in rooms)
+        {
             vertices.Add(new Vertex<Room>((Vector3)room.bounds.position + ((Vector3)room.bounds.size) / 2, room));
         }
 
         delaunay = Delaunay3D.Triangulate(vertices);
     }
 
-    void CreateHallways() {
+    IEnumerator CreateHallways()
+    {
         List<Prim.Edge> edges = new List<Prim.Edge>();
 
-        foreach (var edge in delaunay.Edges) {
+        foreach (var edge in delaunay.Edges)
+        {
             edges.Add(new Prim.Edge(edge.U, edge.V));
         }
 
@@ -123,17 +145,23 @@ public class Generator3D : MonoBehaviour {
         var remainingEdges = new HashSet<Prim.Edge>(edges);
         remainingEdges.ExceptWith(selectedEdges);
 
-        foreach (var edge in remainingEdges) {
-            if (random.NextDouble() < 0.125) {
+        foreach (var edge in remainingEdges)
+        {
+            if (random.NextDouble() < 0.125)
+            {
                 selectedEdges.Add(edge);
             }
         }
+
+        yield return null;
     }
 
-    void PathfindHallways() {
+    IEnumerator PathfindHallways()
+    {
         DungeonPathfinder3D aStar = new DungeonPathfinder3D(size);
 
-        foreach (var edge in selectedEdges) {
+        foreach (var edge in selectedEdges)
+        {
             var startRoom = (edge.U as Vertex<Room>).Item;
             var endRoom = (edge.V as Vertex<Room>).Item;
 
@@ -142,25 +170,34 @@ public class Generator3D : MonoBehaviour {
             var startPos = new Vector3Int((int)startPosf.x, (int)startPosf.y, (int)startPosf.z);
             var endPos = new Vector3Int((int)endPosf.x, (int)endPosf.y, (int)endPosf.z);
 
-            var path = aStar.FindPath(startPos, endPos, (DungeonPathfinder3D.Node a, DungeonPathfinder3D.Node b) => {
+            var path = aStar.FindPath(startPos, endPos, (DungeonPathfinder3D.Node a, DungeonPathfinder3D.Node b) =>
+            {
                 var pathCost = new DungeonPathfinder3D.PathCost();
 
                 var delta = b.Position - a.Position;
 
-                if (delta.y == 0) {
+                if (delta.y == 0)
+                {
                     //flat hallway
                     pathCost.cost = Vector3Int.Distance(b.Position, endPos);    //heuristic
 
-                    if (grid[b.Position] == CellType.Stairs) {
+                    if (grid[b.Position] == CellType.Stairs)
+                    {
                         return pathCost;
-                    } else if (grid[b.Position] == CellType.Room) {
+                    }
+                    else if (grid[b.Position] == CellType.Room)
+                    {
                         pathCost.cost += 5;
-                    } else if (grid[b.Position] == CellType.None) {
+                    }
+                    else if (grid[b.Position] == CellType.None)
+                    {
                         pathCost.cost += 1;
                     }
 
                     pathCost.traversable = true;
-                } else {
+                }
+                else
+                {
                     //staircase
                     if ((grid[a.Position] != CellType.None && grid[a.Position] != CellType.Hallway)
                         || (grid[b.Position] != CellType.None && grid[b.Position] != CellType.Hallway)) return pathCost;
@@ -174,14 +211,16 @@ public class Generator3D : MonoBehaviour {
 
                     if (!grid.InBounds(a.Position + verticalOffset)
                         || !grid.InBounds(a.Position + horizontalOffset)
-                        || !grid.InBounds(a.Position + verticalOffset + horizontalOffset)) {
+                        || !grid.InBounds(a.Position + verticalOffset + horizontalOffset))
+                    {
                         return pathCost;
                     }
 
                     if (grid[a.Position + horizontalOffset] != CellType.None
                         || grid[a.Position + horizontalOffset * 2] != CellType.None
                         || grid[a.Position + verticalOffset + horizontalOffset] != CellType.None
-                        || grid[a.Position + verticalOffset + horizontalOffset * 2] != CellType.None) {
+                        || grid[a.Position + verticalOffset + horizontalOffset * 2] != CellType.None)
+                    {
                         return pathCost;
                     }
 
@@ -192,64 +231,87 @@ public class Generator3D : MonoBehaviour {
                 return pathCost;
             });
 
-            if (path != null) {
-                for (int i = 0; i < path.Count; i++) {
+            if (path != null)
+            {
+                for (int i = 0; i < path.Count; i++)
+                {
                     var current = path[i];
 
-                    if (grid[current] == CellType.None) {
+                    if (grid[current] == CellType.None)
+                    {
                         grid[current] = CellType.Hallway;
                     }
 
-                    if (i > 0) {
+                    if (i > 0)
+                    {
                         var prev = path[i - 1];
 
                         var delta = current - prev;
 
-                        if (delta.y != 0) {
+                        if (delta.y != 0)
+                        {
                             int xDir = Mathf.Clamp(delta.x, -1, 1);
                             int zDir = Mathf.Clamp(delta.z, -1, 1);
                             Vector3Int verticalOffset = new Vector3Int(0, delta.y, 0);
                             Vector3Int horizontalOffset = new Vector3Int(xDir, 0, zDir);
-                            
+
                             grid[prev + horizontalOffset] = CellType.Stairs;
                             grid[prev + horizontalOffset * 2] = CellType.Stairs;
                             grid[prev + verticalOffset + horizontalOffset] = CellType.Stairs;
                             grid[prev + verticalOffset + horizontalOffset * 2] = CellType.Stairs;
 
-                            PlaceStairs(prev + horizontalOffset);
-                            PlaceStairs(prev + horizontalOffset * 2);
-                            PlaceStairs(prev + verticalOffset + horizontalOffset);
-                            PlaceStairs(prev + verticalOffset + horizontalOffset * 2);
+                            yield return StartCoroutine(PlaceStairs(prev + horizontalOffset));
+                            yield return StartCoroutine(PlaceStairs(prev + horizontalOffset * 2));
+                            yield return StartCoroutine(PlaceStairs(prev + verticalOffset + horizontalOffset));
+                            yield return StartCoroutine(PlaceStairs(prev + verticalOffset + horizontalOffset * 2));
                         }
 
                         Debug.DrawLine(prev + new Vector3(0.5f, 0.5f, 0.5f), current + new Vector3(0.5f, 0.5f, 0.5f), Color.blue, 100, false);
                     }
                 }
 
-                foreach (var pos in path) {
-                    if (grid[pos] == CellType.Hallway) {
-                        PlaceHallway(pos);
+                foreach (var pos in path)
+                {
+                    if (grid[pos] == CellType.Hallway)
+                    {
+                        yield return StartCoroutine(PlaceHallway(pos));
                     }
                 }
             }
         }
     }
 
-    void PlaceCube(Vector3Int location, Vector3Int size, Material material) {
+    IEnumerator PlaceCube(Vector3Int location, Vector3Int size, Material material)
+    {
         GameObject go = Instantiate(cubePrefab, location, Quaternion.identity);
         go.GetComponent<Transform>().localScale = size;
         go.GetComponent<MeshRenderer>().material = material;
+
+        // Wait 100ms
+        yield return new WaitForSeconds(0.1f);
     }
 
-    void PlaceRoom(Vector3Int location, Vector3Int size) {
-        PlaceCube(location, size, redMaterial);
+    IEnumerator PlaceRoom(Vector3Int location, Vector3Int size)
+    {
+        yield return StartCoroutine(PlaceCube(location, size, redMaterial));
+
+        // Wait 100ms
+        yield return new WaitForSeconds(0.1f);
     }
 
-    void PlaceHallway(Vector3Int location) {
-        PlaceCube(location, new Vector3Int(1, 1, 1), blueMaterial);
+    IEnumerator PlaceHallway(Vector3Int location)
+    {
+        yield return StartCoroutine(PlaceCube(location, new Vector3Int(1, 1, 1), blueMaterial));
+
+        // Wait 100ms
+        yield return new WaitForSeconds(0.1f);
     }
 
-    void PlaceStairs(Vector3Int location) {
-        PlaceCube(location, new Vector3Int(1, 1, 1), greenMaterial);
+    IEnumerator PlaceStairs(Vector3Int location)
+    {
+        yield return StartCoroutine(PlaceCube(location, new Vector3Int(1, 1, 1), greenMaterial));
+
+        // Wait 100ms
+        yield return new WaitForSeconds(0.1f);
     }
 }
